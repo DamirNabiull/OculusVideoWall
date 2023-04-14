9a16aee8cf91138a04c94e25aaa80712749ed942
Damir Nabiullin <51910382+DamirNabiull@users.noreply.github.com>
2023-04-14 19:15:16 +0300

    Update RestartVideo.cs

--- Assets/Scripts/RestartVideo.cs
+++ Assets/Scripts/RestartVideo.cs
@@ -7,15 +7,28 @@ using UnityEngine.Video;
 public class RestartVideo : MonoBehaviour
 {
     public VideoPlayer player;
+
+    private bool _isPlaying = true;
+    
     private void Update()
     {
         if (OVRInput.Get(OVRInput.RawButton.X))
         {
+            player.Stop();
             player.Play();
         }
         else if (OVRInput.Get(OVRInput.RawButton.Y))
         {
-            player.Stop();
+            if (_isPlaying)
+            {
+                player.Pause();
+                _isPlaying = false;
+            }
+            else
+            {
+                player.Play();
+                _isPlaying = true;
+            }
         }
     }
 }
